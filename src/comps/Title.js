fd66dfb21d9c932aa0972e8919f122872e27ccbf
shivamsh-iitja <2019ucs0100@iitjammu.ac.in>
2021-04-09 22:13:58 +0530

    name change

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>CodeGYM</h1>
+      <h1>codeGYM</h1>
       <h2>Your Pictures</h2>
       <p>A picture is a poem without words.</p>
     </div>
